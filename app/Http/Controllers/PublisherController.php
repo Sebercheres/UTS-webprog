db1156d8261fbfaff890701020c900dd4ebd9ff9
Sebercheres <andrew.willy@binus.ac.id>
2022-11-27 23:05:14 +0700

    final tweak

--- app/Http/Controllers/PublisherController.php
+++ app/Http/Controllers/PublisherController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Models\Publisher;
-use Illuminate\Http\Request;
 
 class PublisherController extends Controller
 {
@@ -18,26 +17,6 @@ class PublisherController extends Controller
         return view('publisher', compact('publishers'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
 
     /**
      * Display the specified resource.
@@ -52,37 +31,4 @@ class PublisherController extends Controller
         return view('publisherdetail', compact('publisher', 'books'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Publisher  $publisher
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Publisher $publisher)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Publisher  $publisher
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Publisher $publisher)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Publisher  $publisher
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Publisher $publisher)
-    {
-        //
-    }
 }
